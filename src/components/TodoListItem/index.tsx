74f9b9b4974de5c2bcda91d5310ae2526bdb4f4c
Damla Koksal <me@damlakoksal.com>
2021-10-24 20:39:26 +0300

    [#6] feat: implement base of TodoList Item component

---
+++ src/components/TodoListItem/index.tsx
@@ -0,0 +1,6 @@
+import { ReactElement } from "react";
+import styles from "./styles.module.scss";
+
+export default function TodoListItem(): ReactElement {
+  return <div className={styles.container}>TodoListItem</div>;
+}
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,19 @@
 import { ReactElement } from "react";
 import Layout from "../../components/Layout";
 import Container from "../../components/Container";
+import TodoList from "../../components/TodoList";
+import TodoListItem from "../../components/TodoListItem";
 
 export default function Home(): ReactElement {
   return (
     <Layout>
-      <Container>test</Container>
+      <Container>
+        <TodoList>
+          <TodoListItem />
+          <TodoListItem />
+          <TodoListItem />
+        </TodoList>
+      </Container>
     </Layout>
   );
 }
