f45ce98017b99b109be80a2abb09900bcc0cfd9a
Damla Koksal <me@damlakoksal.com>
2021-10-24 20:18:42 +0300

    [#6] feat: edit Container component with responsive style

--- src/components/Container/index.tsx
+++ src/components/Container/index.tsx
@@ -1,4 +1,6 @@
 import { ReactElement, ReactNode } from "react";
+import { getCurrentDay } from "../../utils/helpers";
+import Emoji from "../Emoji";
 import styles from "./styles.module.scss";
 
 interface Props {
@@ -6,5 +8,14 @@ interface Props {
 }
 
 export default function Container({ children }: Props): ReactElement {
-  return <div className={styles.container}>{children}</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.header}>
+        <div className={styles.title}>{getCurrentDay()}</div>
+        <Emoji />
+      </div>
+      <div className={styles.body}>{children}</div>
+      <div></div>
+    </div>
+  );
 }
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,13 +1,11 @@
 import { ReactElement } from "react";
 import Layout from "../../components/Layout";
 import Container from "../../components/Container";
-import { useTodos } from "../../contexts/TodoContext";
 
 export default function Home(): ReactElement {
-  const { emoji } = useTodos();
   return (
     <Layout>
-      <Container>{emoji}</Container>
+      <Container>test</Container>
     </Layout>
   );
 }
