957ccda8bc64a875a250c8009544d517776c4681
Damla Koksal <me@damlakoksal.com>
2021-10-24 17:30:56 +0300

    [#6] feat: create Header component

--- src/components/Layout/Header/index.tsx
+++ src/components/Layout/Header/index.tsx
@@ -1,6 +1,10 @@
 import { ReactElement } from "react";
-// import styles from "./styles.module.scss";
+import styles from "./styles.module.scss";
 
 export default function Header(): ReactElement {
-  return <div>Header</div>;
+  return (
+    <div className={styles.container}>
+      <span className={styles.title}>✔&nbsp;To-Do Application&nbsp;✔</span>
+    </div>
+  );
 }
