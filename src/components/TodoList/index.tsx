4d2c5163bfca11b14a93b82698822082a12c2bc2
Damla Koksal <me@damlakoksal.com>
2021-10-24 20:20:48 +0300

    [#6] feat: implement base of TodoList component

---
+++ src/components/TodoList/index.tsx
@@ -0,0 +1,10 @@
+import { ReactElement, ReactNode } from "react";
+import styles from "./styles.module.scss";
+
+interface Props {
+  children: ReactNode;
+}
+
+export default function TodoList({ children }: Props): ReactElement {
+  return <div className={styles.container}>{children}</div>;
+}
