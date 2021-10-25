0d097f7cb3d4606592be0782b2c739eeb2dfc5d9
Damla Koksal <me@damlakoksal.com>
2021-10-25 04:17:48 +0300

    [#6] feat: implement add functionality to Todo and TodoList components

--- src/components/TodoList/index.tsx
+++ src/components/TodoList/index.tsx
@@ -1,24 +1,62 @@
-import { ReactElement } from "react";
+import { ReactElement, useState } from "react";
 import TodoListItem from "../TodoListItem";
 import styles from "./styles.module.scss";
 import { useTodo } from "../../contexts/TodoContext";
+import { nanoid } from "nanoid";
 
 export default function TodoList(): ReactElement {
+  const [task, setTask] = useState("");
   const { todoItems, addTodoItem, removeTodoItem, removeAll, updateTodoItem } =
     useTodo();
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    // TODO: check if it is empty
+    setTask(event.target.value);
+  };
+
+  const handleSubmit = () => {
+    addTodoItem({ id: nanoid(), value: task, done: false });
+    setTask("");
+  };
 
   return (
     <>
       <li className={styles.container}>
+        <div className={styles.add}>
+          <input
+            type="text"
+            id="input"
+            className={styles.field}
+            value={task}
+            placeholder="Enter a task to do"
+            onChange={handleChange}
+          />
+          <input
+            className={styles.submit}
+            type="submit"
+            value="➕"
+            onClick={handleSubmit}
+          />
+        </div>
         {todoItems.map((item, i) => (
           <TodoListItem key={i} item={item} />
         ))}
       </li>
-      <button onClick={() => removeTodoItem(1)}>Remove 1</button>
-      <button onClick={() => updateTodoItem(1, "update edildi.")}>
+      <button onClick={() => removeTodoItem("1")}>Remove 1</button>
+      <button
+        onClick={() =>
+          updateTodoItem("1", { id: "1", value: "test task", done: true })
+        }
+      >
         Update
       </button>
-      <button onClick={() => addTodoItem("test")}>Add</button>
+      <button
+        onClick={() =>
+          addTodoItem({ id: nanoid(), value: "test task", done: false })
+        }
+      >
+        Add
+      </button>
       <button onClick={removeAll}>Reset</button>
     </>
   );
--- src/components/TodoListItem/index.tsx
+++ src/components/TodoListItem/index.tsx
@@ -1,10 +1,36 @@
 import { ReactElement } from "react";
 import styles from "./styles.module.scss";
+import { TodoItem } from "../../interfaces";
+import { useTodo } from "../../contexts/TodoContext";
+import classNames from "classnames";
 
 interface Props {
-  item: string;
+  item: TodoItem;
 }
 
 export default function TodoListItem({ item }: Props): ReactElement {
-  return <div className={styles.container}>{item}</div>;
+  const { updateTodoItem } = useTodo();
+
+  const onValueChange = (): void => {
+    updateTodoItem(item.id, {
+      id: item.id,
+      value: item.value,
+      done: !item.done,
+    });
+  };
+
+  return (
+    <div className={styles.container}>
+      <input
+        className={styles.cb}
+        type="checkbox"
+        name="isDone"
+        checked={item.done}
+        onChange={onValueChange}
+      />
+      <p className={classNames(item.done ? styles.done : styles.notDone)}>
+        {item.value}
+      </p>
+    </div>
+  );
 }
